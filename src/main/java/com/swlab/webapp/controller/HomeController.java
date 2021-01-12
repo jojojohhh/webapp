9feeb7c94cfa9fa4b4f0ca66bc6749cca0fdda11
qwer8714 <37928563+qwer8714@users.noreply.github.com>
2021-01-13 07:19:59 +0900

    view example 추가

--- src/main/java/com/swlab/webapp/controller/HomeController.java
+++ src/main/java/com/swlab/webapp/controller/HomeController.java
@@ -1,14 +1,18 @@
 package com.swlab.webapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/home")
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
-    public String getHome() {
-        return "home";
+    @GetMapping("")
+    public String getHome(Model model) {
+        model.addAttribute("currentPage", "home");
+        return "content/home";
     }
 
 
--- src/main/java/com/swlab/webapp/controller/LoginController.java
+++ src/main/java/com/swlab/webapp/controller/LoginController.java
@@ -1,13 +1,21 @@
 package com.swlab.webapp.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/")
-    public String getLogin() {
-        return "login";
+    @GetMapping("")
+    public String getLogin(@AuthenticationPrincipal User user) {
+        if (user != null) {
+            if (user.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_VIEW"))) {
+                return "redirect:/home";
+            }
+        }
+        return "redirect:/login";
     }
 }
