847f505a37ba36e9ab22e103d3fba7cbe0f08e2a
qwer8714 <qwhtkdwp@gmail.com>
2021-01-19 20:47:01 +0900

    -

--- src/main/java/com/swlab/webapp/config/WebSecurityConfig.java
+++ src/main/java/com/swlab/webapp/config/WebSecurityConfig.java
@@ -35,7 +35,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                    .antMatchers("/", "/login", "/join").permitAll()
+                    .antMatchers("/", "/login", "/join", "/test").permitAll()
                     .antMatchers("/home/users").access("hasRole('ROLE_ADMIN')")
                     .antMatchers("/home", "/home/**").access("hasRole('ROLE_VIEW')")
                     .anyRequest().authenticated()
